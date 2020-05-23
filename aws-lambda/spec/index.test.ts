d0a5cb8c65bf281d3dd4c8714d38a6c788143a5a
Alex Shubin <alexshubin22@gmail.com>
2020-05-23 07:51:43 +0200

    aws lambda exercise

--- aws-lambda/spec/index.test.ts
+++ aws-lambda/spec/index.test.ts
@@ -3,7 +3,18 @@ import mockEvent from './fixtures/event'
 import mockContext from './fixtures/context'
 
 it("returns expected response", async () => {
-  const result = await handler(mockEvent, mockContext)
+  const expectedResult = {
+    "statusCode": 200,
+    "body": "{\"user\":{\"id\":\"abc123\",\"email\":\"abc123@email.com\"}}"
+  }
+  const expectedEvent = {
+    ...mockEvent,
+    "pathParameters": {
+      ...mockEvent.pathParameters,
+      "user_id": "abc123"
+    }
+  }
+  const result = await handler(expectedEvent, mockContext)
 
-  expect(result).toEqual(null)
+  expect(result).toEqual(expectedResult)
 })
